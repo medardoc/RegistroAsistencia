390bf95c92f314af2395cdde7a0b4008c98506f9
medardoc <m.carpio2@alumnos.santotomas.cl>
2024-09-13 15:37:38 -0300

    Archivos y Elementos Visuales
    En este Commit, se agregaron elemntos nuevos como las vistas

--- RegistroAsistencia/Program.cs
+++ RegistroAsistencia/Program.cs
@@ -16,7 +16,7 @@ namespace RegistroAsistencia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Vista1());
         }
     }
 }
---
+++ RegistroAsistencia/Vista4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroAsistencia
+{
+    public partial class Vista4 : Form
+    {
+        public Vista4()
+        {
+            InitializeComponent();
+        }
+    }
+}
